88f3025b841e97e9d932bf3984b8a63704ee7320
ethanduncan <ethan.duncan-stewart@depop.com>
2020-04-23 17:10:28 +0100

    options with lift

--- scala-red-book/src/main/scala/scala/Chapter4.scala
+++ scala-red-book/src/main/scala/scala/Chapter4.scala
@@ -41,6 +41,20 @@ object Chapter4 {
       }
     }
 
+    def variance(xs: Seq[Double]): Option[Double] = {
+      def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum/xs.length)
+      mean(xs).flatMap(mn => mean(xs.map(x => math.pow(x - mn, 2.0))))
+    }
+
+    def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
+
+//    def lift2[A,B,C](f: (A,B) => C): (Option[A], Option[B]) => Option[C] =
+
+    def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+      a.flatMap(x => b.map(y => f(x,y)))
+    }
+
+
   }
   case class Some[+A](get: A) extends Option[A]
   case object None extends Option[Nothing]
--- scala-red-book/src/test/scala/scala/Chapter4Spec.scala
+++ scala-red-book/src/test/scala/scala/Chapter4Spec.scala
@@ -2,11 +2,18 @@ package scala
 
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.{Option => _, Either => _, _}
+
+
 class Chapter4Spec extends WordSpec with Matchers {
 
   "Chapter4 Option.map" should {
     "map a function over an option" in {
-      Chapter4.Some(1).map(_ + 1) shouldEqual Chapter4.Some(2)
+      Some(1).map(_ + 1) shouldEqual Some(2)
+    }
+
+    "map2" in {
+      Option.map2(Some(2),Some(3))(_ + _) shouldEqual Some(5)
     }
   }
 
